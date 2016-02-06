7780fbea19c90f97094089b174aa7e6f9d249476
ddenis <rigel.cyrosea@gmail.com>
2016-02-06 16:51:36 +0200

    Add some global parameters, add threshold weights

--- project_1_template.py
+++ project_1_template.py
@@ -55,11 +55,17 @@ from scipy import linalg,stats
 # import gui_template as gt
 
 
+#Parameters
+
 # Allow switching nonlinearities
 def tanh_derivative(x):
-    return 1 - np.square(np.tanh(x))
+    return np.square(np.tanh(x)) + (-1)
 nonlinear = np.tanh
 nonlinear_derivative = tanh_derivative
+
+train_size = 5000
+test_size = 5000
+learning_rate = 0.5
 
 # Initialize the corresponding networks
 def init_feedforward_classifier(initialization_params):
@@ -69,14 +75,23 @@ def init_feedforward_classifier(initialization_params):
     max_layer_size = layer_sizes.max()
 
     # Initially, no neurons are firing, so state is all zeros
-    feedforward_classifier_state = np.zeros((num_layers, max_layer_size))
+    # Except for thresholds, which we'll put 0.1 in for now.
+    neuron_states = np.zeros((num_layers, max_layer_size))
+    threshold_values = np.ones(num_layers - 1)
+    feedforward_classifier_state = [neuron_states, threshold_values]
 
-    # Initialize classifier connections.  Use a 3D array, layers x inputs x outputs.
-    # Also set connections for neurons which do not exist to 0
-    feedforward_classifier_connections = np.random.random((num_layers - 1, max_layer_size, max_layer_size))
-    for l in np.arange(layer_sizes.shape[0] - 1):
-        feedforward_classifier_connections[l][layer_sizes[l]:max_layer_size] = 0
-        feedforward_classifier_connections[l][:][layer_sizes[l+1]:max_layer_size] = 0
+    # Initialize classifier weights.  Use a 3D array, layers x inputs x outputs.
+    # Threshold weights too, but only a 2D needed for those
+    # Also set weights for neurons which do not exist to 0
+    # Since those weights will never contribute to sums in subsequent layers,
+    # backpropagation should never cause them to change
+    layer_weights = np.random.random((num_layers - 1, max_layer_size, max_layer_size))
+    threshold_weights = np.random.random((num_layers - 1, max_layer_size))
+    for l in np.arange(num_layers - 1):
+        layer_weights[l][layer_sizes[l]:max_layer_size] = 0
+        layer_weights[l][:][layer_sizes[l+1]:max_layer_size] = 0
+        threshold_weights[l][layer_sizes[l]:max_layer_size] = 0
+    feedforward_classifier_connections = [layer_weights, threshold_weights]
 
     return [feedforward_classifier_state, feedforward_classifier_connections]
 
@@ -93,9 +108,16 @@ def init_autoencoder_classifier(initialization_params):
 # Given an input, these functions calculate the corresponding output to 
 # that input.
 def update_feedforward_classifier(feedforward_classifier_state, feedforward_classifier_connections):
+    neuron_states = feedforward_classifier_state[0]
+    threshold_values = feedforward_classifier_state[1]
+    layer_weights = feedforward_classifier_connections[0]
+    threshold_weights = feedforward_classifier_connections[1]
+
     for l in np.arange(1, feedforward_classifier_state.shape[0]):
-        feedforward_classifier_state[l] = nonlinear(feedforward_classifier_connections[l-1]
-                                                    .dot(feedforward_classifier_state[l-1]))
+        input_sum = layer_weights[l-1].dot(neuron_states[l-1])
+        input_sum += threshold_values[l-1] * threshold_weights[l-1]
+        input_sum = nonlinear(input_sum)
+
     return feedforward_classifier_state
     
 def update_autoencoder(autoencoder_state, autoencoder_connections):
@@ -115,13 +137,14 @@ def train_feedforward_classifier(feedforward_classifier_state, feedforward_class
     num_runs = training_params[0]
     data = training_data[0]
     labels = training_data[1]
+    neuron_states = feedforward_classifier_state[0]
 
     # Stochastic gradient descent
     for i in np.arange(num_runs):
         rand_index = np.random.randint(0, data.shape[0])
         datum = data[rand_index]
         label = labels[rand_index]
-        feedforward_classifier_state[0] = datum
+        neuron_states[0] = datum
         update_feedforward_classifier(feedforward_classifier_state, feedforward_classifier_connections)
         feedforward_classifier_connections = backpropagate_feedforward_classifier(feedforward_classifier_state, feedforward_classifier_connections, label)
 
@@ -204,9 +227,9 @@ if __name__=='__main__':
     
     
     # Read data here
-    full_mnist_data, full_mnist_labels = mnist.read_mnist_training_data(10000)
-    training_data = [full_mnist_data[:5000], full_mnist_labels[:5000]]
-    test_data = [full_mnist_data[5000:], full_mnist_labels[5000:]]
+    full_mnist_data, full_mnist_labels = mnist.read_mnist_training_data(train_size + test_size)
+    training_data = [full_mnist_data[:train_size], full_mnist_labels[:train_size]]
+    test_data = [full_mnist_data[train_size:], full_mnist_labels[train_size:]]
     
     # You may also use the gui_template.py functions to collect image data from the user. eg:
     # training_data = gt.get_images()
@@ -217,7 +240,7 @@ if __name__=='__main__':
     # parameters below automatically.
     
     # Initialize network(s) here
-    input_size = 28 * 28  # Pixels in the image
+    input_size = (28 * 28)  # Pixels in the image
     output_size = 10  # Possible classifications
     layer_sizes = np.asarray([input_size, np.sqrt(input_size), output_size])
     initialization_params = [layer_sizes]
